d9374b2f1f0f974128d8dee3e90b03ffa89076cb
Ashok <ashokitschool@gmail.com>
2021-07-20 12:29:04 +0530

    Added SOP stmts

--- src/main/java/in/ashokit/MyApp.java
+++ src/main/java/in/ashokit/MyApp.java
@@ -3,7 +3,8 @@ package in.ashokit;
 public class MyApp {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to my product store...");
+		System.out.println("Welcome to my product store...!!");
+		System.out.println("We have many offers...");
 	}
 
 }
